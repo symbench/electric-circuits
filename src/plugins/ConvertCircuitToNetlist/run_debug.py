35810bfcedb1ba79f98c3692a86102cb15b919d6
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2021-02-21 22:25:42 -0600

    WIP- Run black on plugin files

--- src/plugins/ConvertCircuitToNetlist/run_debug.py
+++ src/plugins/ConvertCircuitToNetlist/run_debug.py
@@ -17,33 +17,40 @@ import logging
 from webgme_bindings import WebGME
 from ConvertCircuitToNetlist import ConvertCircuitToNetlist
 
-logger = logging.getLogger('ConvertCircuitToNetlist')
+logger = logging.getLogger("ConvertCircuitToNetlist")
 
 # Modify these or add option or parse from sys.argv (as in done in run_plugin.py)
-PORT = '5555'
-PROJECT_NAME = 'Example'
-BRANCH_NAME = 'master'
-ACTIVE_NODE_PATH = ''
+PORT = "5555"
+PROJECT_NAME = "Example"
+BRANCH_NAME = "master"
+ACTIVE_NODE_PATH = ""
 ACTIVE_SELECTION_PATHS = []
-NAMESPACE = ''
-METADATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'metadata.json')
+NAMESPACE = ""
+METADATA_PATH = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "metadata.json"
+)
 
-COREZMQ_SERVER_FILE = os.path.join(os.getcwd(), 'node_modules', 'webgme-bindings', 'bin', 'corezmq_server.js')
+COREZMQ_SERVER_FILE = os.path.join(
+    os.getcwd(), "node_modules", "webgme-bindings", "bin", "corezmq_server.js"
+)
 
 if not os.path.isfile(COREZMQ_SERVER_FILE):
-    COREZMQ_SERVER_FILE = os.path.join(os.getcwd(), 'bin', 'corezmq_server.js')
+    COREZMQ_SERVER_FILE = os.path.join(os.getcwd(), "bin", "corezmq_server.js")
 
 # Star the server (see bin/corezmq_server.js for more options e.g. for how to pass a pluginConfig)
-node_process = subprocess.Popen(['node', COREZMQ_SERVER_FILE, PROJECT_NAME, '-p', PORT, '-m', METADATA_PATH],
-                                stdout=sys.stdout, stderr=sys.stderr)
+node_process = subprocess.Popen(
+    ["node", COREZMQ_SERVER_FILE, PROJECT_NAME, "-p", PORT, "-m", METADATA_PATH],
+    stdout=sys.stdout,
+    stderr=sys.stderr,
+)
 
-logger.info('Node-process running at PID {0}'.format(node_process.pid))
+logger.info("Node-process running at PID {0}".format(node_process.pid))
 # Create an instance of WebGME and the plugin
 webgme = WebGME(PORT, logger)
 
 
 def exit_handler():
-    logger.info('Cleaning up!')
+    logger.info("Cleaning up!")
     webgme.disconnect()
     node_process.send_signal(signal.SIGTERM)
 
@@ -51,7 +58,14 @@ def exit_handler():
 atexit.register(exit_handler)
 
 commit_hash = webgme.project.get_branch_hash(BRANCH_NAME)
-plugin = ConvertCircuitToNetlist(webgme, commit_hash, BRANCH_NAME, ACTIVE_NODE_PATH, ACTIVE_SELECTION_PATHS, NAMESPACE)
+plugin = ConvertCircuitToNetlist(
+    webgme,
+    commit_hash,
+    BRANCH_NAME,
+    ACTIVE_NODE_PATH,
+    ACTIVE_SELECTION_PATHS,
+    NAMESPACE,
+)
 
 # Do the work
 plugin.main()
--- src/plugins/ConvertCircuitToNetlist/run_plugin.py
+++ src/plugins/ConvertCircuitToNetlist/run_plugin.py
@@ -12,10 +12,10 @@ import logging
 from webgme_bindings import WebGME
 from ConvertCircuitToNetlist import ConvertCircuitToNetlist
 
-logger = logging.getLogger('ConvertCircuitToNetlist')
+logger = logging.getLogger("ConvertCircuitToNetlist")
 
 # Read in the context from sys.argv passed by the plugin
-logger.info('sys.args: {0}'.format(sys.argv))
+logger.info("sys.args: {0}".format(sys.argv))
 
 PORT = sys.argv[1]
 COMMIT_HASH = sys.argv[2].strip('"')
@@ -24,21 +24,28 @@ ACTIVE_NODE_PATH = sys.argv[4].strip('"')
 ACTIVE_SELECTION_PATHS = []
 
 if sys.argv[5] != '""':
-    ACTIVE_SELECTION_PATHS = sys.argv[5].strip('"').split(',')
-    if ACTIVE_SELECTION_PATHS[0] == '':
+    ACTIVE_SELECTION_PATHS = sys.argv[5].strip('"').split(",")
+    if ACTIVE_SELECTION_PATHS[0] == "":
         ACTIVE_SELECTION_PATHS.pop(0)
 
 NAMESPACE = sys.argv[6].strip('"')
 
-logger.debug('commit-hash: {0}'.format(COMMIT_HASH))
-logger.debug('branch-name: {0}'.format(BRANCH_NAME))
-logger.debug('active-node-path: {0}'.format(ACTIVE_NODE_PATH))
-logger.debug('active-selection-paths: {0}'.format(ACTIVE_SELECTION_PATHS))
-logger.debug('name-space: {0}'.format(NAMESPACE))
+logger.debug("commit-hash: {0}".format(COMMIT_HASH))
+logger.debug("branch-name: {0}".format(BRANCH_NAME))
+logger.debug("active-node-path: {0}".format(ACTIVE_NODE_PATH))
+logger.debug("active-selection-paths: {0}".format(ACTIVE_SELECTION_PATHS))
+logger.debug("name-space: {0}".format(NAMESPACE))
 
 # Create an instance of WebGME and the plugin
 webgme = WebGME(PORT, logger)
-plugin = ConvertCircuitToNetlist(webgme, COMMIT_HASH, BRANCH_NAME, ACTIVE_NODE_PATH, ACTIVE_SELECTION_PATHS, NAMESPACE)
+plugin = ConvertCircuitToNetlist(
+    webgme,
+    COMMIT_HASH,
+    BRANCH_NAME,
+    ACTIVE_NODE_PATH,
+    ACTIVE_SELECTION_PATHS,
+    NAMESPACE,
+)
 
 # Do the work
 plugin.main()
