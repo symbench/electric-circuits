f24a7138048f0566a46ba6d09448b2ff4a727905
Brian Broll <brian.broll@gmail.com>
2021-03-02 16:04:31 -0600

    Remove pyspice_circuit data attribute. Closes #67

--- src/plugins/ConvertCircuitToNetlist/ConvertCircuitToNetlist/__init__.py
+++ src/plugins/ConvertCircuitToNetlist/ConvertCircuitToNetlist/__init__.py
@@ -19,9 +19,9 @@ PluginBase = getattr(base_module, BASE_PLUGIN_NAME)
 
 class ConvertCircuitToNetlist(PluginBase):
     def main(self) -> None:
-        super().convert_to_pyspice(self.active_node)
+        circuit = super().convert_to_pyspice(self.active_node)
         output_filename = self.get_current_config().get("file_name")
         if not output_filename:
             output_filename = self.core.get_attribute(self.active_node, "name")
-        self.add_file(f"{output_filename}.cir", str(self.pyspice_circuit))
+        self.add_file(f"{output_filename}.cir", str(circuit))
         self.result_set_success(True)
--- src/plugins/RecommendNextComponentsMock/RecommendNextComponentsMock/__init__.py
+++ src/plugins/RecommendNextComponentsMock/RecommendNextComponentsMock/__init__.py
@@ -20,15 +20,15 @@ PluginBase = getattr(base_module, BASE_PLUGIN_NAME)
 class RecommendNextComponentsMock(PluginBase):
     """Runs a mock implementation for recommending components to be added to the Circuit"""
 
-    def run_analytics(self) -> None:
-        self._recommend_next_components()
+    def run_analytics(self, circuit) -> None:
+        self._recommend_next_components(circuit)
 
-    def _recommend_next_components(self) -> None:
+    def _recommend_next_components(self, circuit) -> None:
         component_counts = Counter(
-            type(element).__name__ for element in self.pyspice_circuit.elements
+            type(element).__name__ for element in circuit.elements
         )
 
-        for subckt in self.pyspice_circuit.subcircuits:
+        for subckt in circuit.subcircuits:
             component_counts.update(
                 type(element).__name__ for element in subckt.elements
             )
