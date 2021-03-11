22f357b987701466676336ba95047c013dc39b7c
Brian Broll <brian.broll@vanderbilt.edu>
2021-03-10 19:35:54 -0600

    Sort recommendations by likelihood. Closes #88 (#89)
    * Sort recommendations by likelihood. Closes #88
    
    * update example recommender
    
    * WIP- Fix test for example model recommender
    
    Co-authored-by: Umesh Timalsina <umesh.timalsina@vanderbilt.edu>

--- src/plugins/RecommendNextComponents/RecommendNextComponents/models/example/__init__.py
+++ src/plugins/RecommendNextComponents/RecommendNextComponents/models/example/__init__.py
@@ -10,10 +10,9 @@ def analyze(circuit: Union[Circuit, SubCircuit]):
     for subckt in circuit.subcircuits:
         component_counts.update(type(element).__name__ for element in subckt.elements)
 
-    most_common = dict(component_counts.most_common(3))
+    total = sum(component_counts.values())
 
-    recommendations = [
-        {"element": element, "confidence": f"{ count / 50 * 100}%"}
-        for element, count in most_common.items()
-    ]
+    recommendations = dict(
+        ((element, count / total) for (element, count) in component_counts.items())
+    )
     return recommendations
--- test/plugins/RecommendNextComponents/RecommendNextComponents.spec.js
+++ test/plugins/RecommendNextComponents/RecommendNextComponents.spec.js
@@ -65,7 +65,7 @@ describe('RecommendNextComponents', function () {
         Object.keys(testFixture.CIRCUITS).forEach(cktName => {
             it(`Should recommend next 3 components for ${cktName}`, async () => {
                 const recommendations = await runPluginAndGetRecommendations(testFixture.CIRCUITS[cktName]);
-                assert(recommendations.length <= 3);
+                assert(Object.keys(recommendations).length);
             });
         });
     });
