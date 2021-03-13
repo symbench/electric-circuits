6576767b28e7315fc2c40a49f2b5b12987ac752c
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2021-03-13 00:14:51 -0600

    WIP- Clone GraphGym to $HOME

--- utils/install-graphgym.sh
+++ utils/install-graphgym.sh
@@ -1,7 +1,8 @@
-#!/usr/bin env bash
-git clone https://github.com/symbench/GraphGym
-cd GraphGym
+#!/usr/bin/env bash
+rm -rf ~/GraphGym
+git clone https://github.com/symbench/GraphGym ~/GraphGym
+cd ~/GraphGym
 # Temporary until PR is merged
 git checkout -b 1-py38-install origin/1-py38-install
 python setup.py install
-
+cd -
