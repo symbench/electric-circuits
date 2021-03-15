8c87f66ca59909b879a0057a4324acdd98a621ff
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2021-03-15 09:13:23 -0500

    WIP- Remove branch checkout; Add env for $CUDA=cpu

--- utils/install-graphgym.sh
+++ utils/install-graphgym.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
+export CUDA=cpu
 rm -rf ~/GraphGym
 git clone https://github.com/symbench/GraphGym ~/GraphGym
 cd ~/GraphGym
-# Temporary until PR is merged
-git checkout -b 1-py38-install origin/1-py38-install
 python setup.py install
 cd -
