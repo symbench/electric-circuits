2843b84cb5346231a5992a71fb201c5450115d25
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2021-03-16 16:04:13 -0500

    Change PySpice to GME Type in RecommendNextComponents. Closes #111

--- src/plugins/RecommendNextComponents/RecommendNextComponents/__init__.py
+++ src/plugins/RecommendNextComponents/RecommendNextComponents/__init__.py
@@ -25,6 +25,40 @@ def import_from_path(path, module_name):
 base_module = import_from_path(BASE_PLUGIN_PATH, IMPORT_MODULE_NAME)
 PluginBase = getattr(base_module, BASE_PLUGIN_NAME)
 
+PYSPICE_TO_GME_TYPE = {
+    "SubCircuitElement": "Circuit",
+    "Resistor": "Resistor",
+    "SemiconductorResistor": "Resistor",
+    "BehavioralResistor": "Resistor",
+    "Capacitor": "Capacitor",
+    "SemiconductorCapacitor": "Capacitor",
+    "BehavioralCapacitor": "Capacitor",
+    "Inductor": "Inductor",
+    "BehavioralInductor": "Inductor",
+    "CoupledInductor": "Inductor",
+    "VoltageControlledSwitch": None,
+    "CurrentControlledSwitch": None,
+    "VoltageSource": "Voltage",
+    "CurrentSource": "Current",
+    "VoltageControlledCurrentSource": "VCC",
+    "VoltageControlledVoltageSource": "VCV",
+    "CurrentControlledCurrentSource": "CCC",
+    "CurrentControlledVoltageSource": "CCV",
+    "NonLinearVoltageSource": None,
+    "NonLinearCurrentSource": None,
+    "Diode": "Diode",
+    "BipolarJunctionTransistor": ["NPN", "PNP"],
+    "JunctionFieldEffectTransistor": None,
+    "Mesfet": None,
+    "Mosfet": ["NMOS", "PMOS"],
+    "LosslessTransmissionLine": None,
+    "LossyTransmission": None,
+    "CoupledMulticonductorLine": None,
+    "UniformDistributedRCLine": None,
+    "SingleLossyTransmissionLine": None,
+    "XSpiceElement": None,
+}
+
 
 def load_model(name):
     model_path = Path(f"{script_dir}/models/{name}/__init__.py").resolve()
@@ -43,5 +77,22 @@ class RecommendNextComponents(PluginBase):
     def run_analytics(self, circuit: Union[Circuit, SubCircuit]) -> None:
         model_name = self.get_current_config().get("model")
         model = load_model(model_name)
-        recommendations = sort_dict(model.analyze(circuit))
-        self.add_file("recommendations.json", json.dumps(recommendations, indent=2))
+        recommendations = self._pyspice_to_gme_type(model.analyze(circuit))
+        recommendations = sort_dict(recommendations)
+        self.add_file(
+            "recommendations.json",
+            json.dumps(recommendations, indent=2),
+        )
+
+    @staticmethod
+    def _pyspice_to_gme_type(recommendations: dict) -> dict:
+        gme_recommendations = {}
+        for pyspice_type, confidence in recommendations.items():
+            if pyspice_type in PYSPICE_TO_GME_TYPE:
+                if isinstance(recs := PYSPICE_TO_GME_TYPE[pyspice_type], list):
+                    for rec in recs:
+                        gme_recommendations[rec] = confidence
+                else:
+                    gme_recommendations[PYSPICE_TO_GME_TYPE[pyspice_type]] = confidence
+
+        return gme_recommendations
