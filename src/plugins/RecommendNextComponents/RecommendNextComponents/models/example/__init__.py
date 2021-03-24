43660fd51d1255b1c59a1fc3919c149d810b0959
Brian Broll <brian.broll@gmail.com>
2021-03-24 15:39:16 -0500

    Update output format of the example recommender

--- src/plugins/RecommendNextComponents/RecommendNextComponents/models/example/__init__.py
+++ src/plugins/RecommendNextComponents/RecommendNextComponents/models/example/__init__.py
@@ -1,18 +1,32 @@
 from collections import Counter
+from itertools import chain
 from typing import Union
 
 from PySpice.Spice.Netlist import Circuit, SubCircuit
 
 
-def analyze(circuit: Union[Circuit, SubCircuit]):
-    component_counts = Counter(type(element).__name__ for element in circuit.elements)
+def new_element(element_cls, circuit: Union[Circuit, SubCircuit]):
+    existing = next((element for element in all_elements(circuit)))
+    return {
+        "type": element_cls.__name__,
+        "pins": [pin.node.name for pin in existing.pins],
+    }
 
-    for subckt in circuit.subcircuits:
-        component_counts.update(type(element).__name__ for element in subckt.elements)
+
+def all_elements(circuit: Union[Circuit, SubCircuit]):
+    return chain(
+        (element for element in circuit.elements),
+        *[all_elements(subckt) for subckt in circuit.subcircuits],
+    )
+
+
+def analyze(circuit: Union[Circuit, SubCircuit]):
+    component_counts = Counter(type(element) for element in all_elements(circuit))
 
     total = sum(component_counts.values())
 
-    recommendations = dict(
-        ((element, count / total) for (element, count) in component_counts.items())
-    )
+    recommendations = [
+        (new_element(element, circuit), count / total)
+        for (element, count) in component_counts.items()
+    ]
     return recommendations
