6e05e8d61b742eea7261232b24d14f92d032b554
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2021-04-08 18:58:01 -0500

    Fix python error in deployment by installing spice-completion. Closes #154

--- utils/install-graphgym.sh
+++ utils/install-graphgym.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 export CUDA=cpu
 rm -rf /tmp/GraphGym
+rm -rf /tmp/spice-completion
+
 git clone https://github.com/symbench/GraphGym /tmp/GraphGym
 cd /tmp/GraphGym
 python setup.py install
+cd -
+
+git clone https://github.com/symbench/spice-completion /tmp/spice-completion
+cd /tmp/spice-completion
+python -m pip install .
 cd -
